1917916aae225fce5e0cb12128d0d4cee8a3d48d
Липин Михаил <lipin_mm@nlmk.com>
2024-08-25 18:00:42 +0300

    create function for table highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,28 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    for (let j = 0; j < table.rows[i].cells.length; j++) {
+
+      if (table.rows[i].cells[j].cellIndex === 2 && table.rows[i].cells[j].textContent === 'm') {
+        table.rows[i].classList.add('male');
+      } else if (table.rows[i].cells[j].cellIndex === 2 && table.rows[i].cells[j].textContent === 'f') {
+        table.rows[i].classList.add('female');
+      }
+
+      if (table.rows[i].cells[j].cellIndex === 1 && Number(table.rows[i].cells[j].textContent) < 18) {
+        table.rows[i].style.textDecoration = 'line-through';
+      }
+
+      if (table.rows[i].cells[j].cellIndex === 3 && table.rows[i].cells[j].dataset.available === 'true') {
+        table.rows[i].classList.add('available');
+      } else if (table.rows[i].cells[j].cellIndex === 3 && table.rows[i].cells[j].dataset.available === 'false') {
+        table.rows[i].classList.add('unavailable');
+      }
+
+      if (table.rows[i].rowIndex !== 0 && ( table.rows[i].cells[j].dataset.available === 'true' )) {
+        continue;
+      } else if(table.rows[i].rowIndex !== 0 && (table.rows[i].cells[j].dataset.available === 'false' || table.rows[i].cells[j].cellIndex + 1 === table.rows[i].cells.length)) {
+        table.rows[i].setAttribute('hidden', 'true');
+      }
+    }
+  }
 }
