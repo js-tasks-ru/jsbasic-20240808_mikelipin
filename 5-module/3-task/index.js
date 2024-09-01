8565a0729a8d3c7814da81eae664661358f45335
Липин Михаил <lipin_mm@nlmk.com>
2024-09-01 23:45:27 +0300

    Learning project: Create Carousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,37 @@
 function initCarousel() {
-  // ваш код...
+
+  const carouselWrapper = document.querySelector('[data-carousel-holder]');
+  const carouselInner = carouselWrapper.querySelector('.carousel__inner');
+  const carouselArrowRight = carouselWrapper.querySelector('.carousel__arrow_right');
+  const carouselArrowLeft = carouselWrapper.querySelector('.carousel__arrow_left');
+
+  let currentSlide = 0;
+  
+  carouselArrowRight.addEventListener('click', () => {
+    if (currentSlide < carouselInner.children.length - 1) {
+      currentSlide += 1;
+      carouselInner.style.transform = `translateX(-${currentSlide * 500}px)`;
+    }
+    if (currentSlide === carouselInner.children.length - 1) {
+      carouselArrowRight.style.display = 'none';
+    }
+    if (currentSlide !== 0) {
+      carouselArrowLeft.style.display = '';
+    }
+  });
+
+  carouselArrowLeft.addEventListener('click', () => {
+    if (currentSlide > 0) {
+      currentSlide -= 1;
+      carouselInner.style.transform = `translateX(-${currentSlide * 500}px)`;
+    }
+    if (currentSlide === 0) {
+      carouselArrowLeft.style.display = 'none';
+    }
+    if (currentSlide !== carouselInner.children.length - 1) {
+      carouselArrowRight.style.display = '';
+    }
+  });
+  
+  carouselArrowLeft.style.display = 'none';
 }
