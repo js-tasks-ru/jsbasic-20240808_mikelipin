d866ebba7cffc006e3676f39a25853b370a58e7a
Липин Михаил <lipin_mm@nlmk.com>
2024-09-05 10:14:31 +0300

    possibility to add table with deletable rows

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,41 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+    this.elem.className = 'user-table';
+
+    this.elem.innerHTML = `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    `;
+
+    for (const row of rows) {
+      const tr = document.createElement('tr');
+      for (const key in row) {
+        const td = document.createElement('td');
+        td.textContent = row[key];
+        tr.appendChild(td);
+      }
+
+      const td = document.createElement('td');
+      const button = document.createElement('button');
+      button.textContent = 'X';
+      td.appendChild(button);
+      tr.appendChild(td);
+      this.elem.querySelector('tbody').appendChild(tr);
+    }
+
+    this.elem.addEventListener('click', (event) => {
+      const tr = event.target.closest('tr');
+      tr.remove();
+    });
   }
 }
